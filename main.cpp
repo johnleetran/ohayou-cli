21206866f41c5a2bf82378e79a7f8b7ecc4f4a5d
John Tran <johnleetran@yahoo.com>
2021-04-15 22:40:54 -0700

    better cartoon

--- main.cpp
+++ main.cpp
@@ -27,40 +27,64 @@
 
 using namespace cv;
 using namespace std;
-const int slider_max = 50;
+const int slider_max = 100;
 int slider = 1;
-Mat gImage;
+Mat gImage, gSaturationImg, gVibranceImg;
 
-void on_trackbar(int x, void *data)
+void on_trackbar(int x, void* data)
 {
     int val = x;
-    if(val != 0){
-        int scale = 1;
-        int saturation = val;
-        Mat output_image, saturated;
-        Mat gray, gray_blur, mask;
-        Mat color_blur_output;
-        Mat hsv_img_channels[3], hsv_img;
-        cv::cvtColor(gImage, gray, cv::COLOR_BGR2GRAY);
-        cv::medianBlur(gray, gray_blur, 5);
-        cv::adaptiveThreshold(gray_blur, mask, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 9, 5);
+    /** EDGES **/
+    // Apply median filter to remove possible noise
+    Mat imgMedian;
+    medianBlur(gImage, imgMedian, 7);
 
-        cv::bilateralFilter(gImage, color_blur_output, 9, 200, 200);
+    // Detect edges with canny
+    Mat imgCanny;
+    Canny(imgMedian, imgCanny, val, 150);
 
-        cv::bitwise_and(gImage, color_blur_output, output_image, mask);
-        cv::cvtColor(output_image, output_image, cv::COLOR_BGR2HSV);
-        cv::split(output_image, hsv_img_channels);
-        hsv_img_channels[1] *= saturation * 0.5;
-        cv::merge(hsv_img_channels, 3, hsv_img);
+    // Dilate the edges
+    Mat kernel = getStructuringElement(MORPH_RECT, Size(2, 2));
+    dilate(imgCanny, imgCanny, kernel);
 
-        cv::cvtColor(hsv_img, output_image, cv::COLOR_HSV2BGR);
-        imshow("image", output_image);
+    // Scale edges values to 1 and invert values
+    imgCanny = imgCanny / 255;
+    imgCanny = 1 - imgCanny;
 
-        // s = s *satadj
-        //     s = np.clip(s, 0, 255)
-        //             imghsv = cv2.merge([ h, s, v ])
-        //                          
-    }
+    // Use float values to allow multiply between 0 and 1
+    Mat imgCannyf;
+    imgCanny.convertTo(imgCannyf, CV_32FC3);
+
+    // Blur the edgest to do smooth effect
+    blur(imgCannyf, imgCannyf, Size(5, 5));
+
+    /** COLOR **/
+    // Apply bilateral filter to homogenizes color
+    Mat imgBF;
+    bilateralFilter(gImage, imgBF, 9, 150.0, 150.0);
+
+    // truncate colors
+    Mat result = imgBF / 25;
+    result = result * 25;
+
+    /** MERGES COLOR + EDGES **/
+    // Create a 3 channles for edges
+    Mat imgCanny3c;
+    Mat cannyChannels[] = {imgCannyf, imgCannyf, imgCannyf};
+    merge(cannyChannels, 3, imgCanny3c);
+
+    // Convert color result to float
+    Mat resultf;
+    result.convertTo(resultf, CV_32FC3);
+
+    // Multiply color and edges matrices
+    multiply(resultf, imgCanny3c, resultf);
+
+    // convert to 8 bits color
+    resultf.convertTo(result, CV_8UC3);
+
+    // Show image
+    imshow("image", result);
     // Mat output_image = ohayou::gaussian_blur(img, 9);
     // Mat output_image = ohayou::horizontal_motion_blur(img, val);
     // Mat output_image = ohayou::apply_sepia(img, val/100.0);
@@ -77,13 +101,14 @@ void on_trackbar(int x, void *data)
 
 }
 
+
 void execute(Mat &img)
 {
     cv::namedWindow("image");
     cv::createTrackbar("val", "image", &slider, slider_max, on_trackbar);
 
     float val = cv::getTrackbarPos("val", "image");
-    gImage= img;
+    gImage = img;
     on_trackbar(val, 0);
 
     cv::waitKey(0);
