ef7bb3c90fca75c4124de1294ab52b80e371aec3
John Tran <johnleetran@yahoo.com>
2021-04-16 23:28:31 -0700

    Lomography

--- main.cpp
+++ main.cpp
@@ -27,80 +27,52 @@
 
 using namespace cv;
 using namespace std;
-const int slider_max = 100;
+const int slider_max = 1000;
 int slider = 1;
 Mat gImage, gSaturationImg, gVibranceImg;
 
-void on_trackbar(int x, void* data)
+void on_trackbar(int x, void *data)
 {
     int val = x;
-    /** EDGES **/
-    // Apply median filter to remove possible noise
-    Mat imgMedian;
-    medianBlur(gImage, imgMedian, 7);
+    Mat output_image;
+    Mat result;
+    Mat img = gImage;
+    const double exponential_e = std::exp(1.0);
+    // Create Look-up table for color curve effect
+    Mat lut(1, 256, CV_8UC1);
+    for (int i = 0; i < 256; i++)
+    {
+        float x = (float)i / 256.0;
+        lut.at<uchar>(i) = cvRound(256 * (1 / (1 + pow(exponential_e, -((x - 0.5) / 0.1)))));
+    }
 
-    // Detect edges with canny
-    Mat imgCanny;
-    Canny(imgMedian, imgCanny, val, 150);
+    // Split the image channels and apply curve transform only to red channel
+    vector<Mat> bgr;
+    split(img, bgr);
+    LUT(bgr[2], lut, bgr[2]);
+    // merge result
+    merge(bgr, result);
 
-    // Dilate the edges
-    Mat kernel = getStructuringElement(MORPH_RECT, Size(2, 2));
-    dilate(imgCanny, imgCanny, kernel);
+    // Create image for halo dark
+    float halo_val = val / 1000.0;
+    Mat halo(img.rows, img.cols, CV_32FC3, Scalar(halo_val, halo_val, halo_val));
+    // Create circle
+    circle(halo, Point(img.cols / 2, img.rows / 2), img.cols / 3, Scalar(1, 1, 1), -1);
+    blur(halo, halo, Size(img.cols / 3, img.cols / 3));
 
-    // Scale edges values to 1 and invert values
-    imgCanny = imgCanny / 255;
-    imgCanny = 1 - imgCanny;
-
-    // Use float values to allow multiply between 0 and 1
-    Mat imgCannyf;
-    imgCanny.convertTo(imgCannyf, CV_32FC3);
-
-    // Blur the edgest to do smooth effect
-    blur(imgCannyf, imgCannyf, Size(5, 5));
-
-    /** COLOR **/
-    // Apply bilateral filter to homogenizes color
-    Mat imgBF;
-    bilateralFilter(gImage, imgBF, 9, 150.0, 150.0);
-
-    // truncate colors
-    Mat result = imgBF / 25;
-    result = result * 25;
-
-    /** MERGES COLOR + EDGES **/
-    // Create a 3 channles for edges
-    Mat imgCanny3c;
-    Mat cannyChannels[] = {imgCannyf, imgCannyf, imgCannyf};
-    merge(cannyChannels, 3, imgCanny3c);
-
-    // Convert color result to float
+    // Convert the result to float to allow multiply by 1 factor
     Mat resultf;
     result.convertTo(resultf, CV_32FC3);
 
-    // Multiply color and edges matrices
-    multiply(resultf, imgCanny3c, resultf);
+    // Multiply our result with halo
+    multiply(resultf, halo, resultf);
 
-    // convert to 8 bits color
+    // convert to 8 bits
     resultf.convertTo(result, CV_8UC3);
 
-    // Show image
+    // show result
     imshow("image", result);
-    // Mat output_image = ohayou::gaussian_blur(img, 9);
-    // Mat output_image = ohayou::horizontal_motion_blur(img, val);
-    // Mat output_image = ohayou::apply_sepia(img, val/100.0);
-    // Mat output_image = ohayou::apply_splash(img,
-    //                                         cv::Scalar(155, 30, 30),
-    //                                         cv::Scalar(315, 255, 255));
-
-    //Mat output_image = ohayou::apply_duo_tone(img, 1, 1 + val / 100.0);
-    //Mat output_image = ohayou::horizontal_motion_blur(img, val);
-    //Mat output_image = ohayou::apply_brightness(img, val / 2.0);
-    //Mat output_image = ohayou::apply_summer(img);
-    //Mat output_image = ohayou::apply_oil_paint(img, val);
-    //Mat output_image = ohayou::apply_white_balance(img, val/100.0);
-
 }
-
 
 void execute(Mat &img)
 {
---
+++ samples/cartoon.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <algorithm>
+
+//opencv
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+//ohayou blurs
+#include "includes/blur/gaussian.hpp"
+#include "includes/blur/average.hpp"
+#include "includes/blur/motion.hpp"
+
+//ohayou photo-filters
+#include "includes/photo-filter/brightness.hpp"
+#include "includes/photo-filter/duo-tone.hpp"
+#include "includes/photo-filter/sepia.hpp"
+#include "includes/photo-filter/splash.hpp"
+#include "includes/photo-filter/oil-paint.hpp"
+#include "includes/photo-filter/white-balance.hpp"
+
+// command-line-parser
+#include "includes/command-line-parser/CLI11.hh"
+
+//ohayou preset
+#include "includes/preset/summer.hpp"
+
+using namespace cv;
+using namespace std;
+const int slider_max = 100;
+int slider = 1;
+Mat gImage, gSaturationImg, gVibranceImg;
+
+void on_trackbar(int x, void* data)
+{
+    int val = x;
+    /** EDGES **/
+    // Apply median filter to remove possible noise
+    Mat imgMedian;
+    medianBlur(gImage, imgMedian, 7);
+
+    // Detect edges with canny
+    Mat imgCanny;
+    Canny(imgMedian, imgCanny, 50, 150);
+
+    // Dilate the edges
+    Mat kernel = getStructuringElement(MORPH_RECT, Size(2, 2));
+    //dilate(imgCanny, imgCanny, kernel);
+
+    // Scale edges values to 1 and invert values
+    imgCanny = imgCanny / 255;
+    imgCanny = 1 - imgCanny;
+
+    // Use float values to allow multiply between 0 and 1
+    Mat imgCannyf;
+    imgCanny.convertTo(imgCannyf, CV_32FC3);
+
+    // Blur the edgest to do smooth effect
+    blur(imgCannyf, imgCannyf, Size(5, 5));
+
+    /** COLOR **/
+    // Apply bilateral filter to homogenizes color
+    Mat imgBF;
+    bilateralFilter(gImage, imgBF, 9, 200.0, 200.0);
+
+    // truncate colors
+    Mat result = imgBF / 25;
+    result = result * 25;
+
+    /** MERGES COLOR + EDGES **/
+    // Create a 3 channles for edges
+    Mat imgCanny3c;
+    Mat cannyChannels[] = {imgCannyf, imgCannyf, imgCannyf};
+    merge(cannyChannels, 3, imgCanny3c);
+
+    // Convert color result to float
+    Mat resultf;
+    result.convertTo(resultf, CV_32FC3);
+
+    // Multiply color and edges matrices
+    multiply(resultf, imgCanny3c, resultf);
+
+    // convert to 8 bits color
+    resultf.convertTo(result, CV_8UC3);
+
+    // Show image
+    imshow("image", result);
+    // Mat output_image = ohayou::gaussian_blur(img, 9);
+    // Mat output_image = ohayou::horizontal_motion_blur(img, val);
+    // Mat output_image = ohayou::apply_sepia(img, val/100.0);
+    // Mat output_image = ohayou::apply_splash(img,
+    //                                         cv::Scalar(155, 30, 30),
+    //                                         cv::Scalar(315, 255, 255));
+
+    //Mat output_image = ohayou::apply_duo_tone(img, 1, 1 + val / 100.0);
+    //Mat output_image = ohayou::horizontal_motion_blur(img, val);
+    //Mat output_image = ohayou::apply_brightness(img, val / 2.0);
+    //Mat output_image = ohayou::apply_summer(img);
+    //Mat output_image = ohayou::apply_oil_paint(img, val);
+    //Mat output_image = ohayou::apply_white_balance(img, val/100.0);
+
+}
+
+
+void execute(Mat &img)
+{
+    cv::namedWindow("image");
+    cv::createTrackbar("val", "image", &slider, slider_max, on_trackbar);
+
+    float val = cv::getTrackbarPos("val", "image");
+    gImage = img;
+    on_trackbar(val, 0);
+
+    cv::waitKey(0);
+}
+
+int main(int argc, char *argv[])
+{
+    std::string file_path = argv[1];
+    std::string image_path = samples::findFile(file_path);
+    Mat img = imread(image_path, IMREAD_COLOR);
+
+    execute(img);
+
+    return 0;
+}
